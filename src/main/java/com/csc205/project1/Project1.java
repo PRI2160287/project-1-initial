4219b5c7da1e0c18dab55957bbb84465adb8635b
PRI2160287 <101784442+PRI2160287@users.noreply.github.com>
2022-03-27 11:36:53 -0700

    Update Project1.java

--- src/main/java/com/csc205/project1/Project1.java
+++ src/main/java/com/csc205/project1/Project1.java
@@ -1,13 +1,63 @@
 package com.csc205.project1;
-
-/**
- * Driver class used to test Point implementation for Project 1.
- *
- * @author Ray Hedgecock
- * @version 1.1
- */
 public class Project1 {
+    static class Point {
+        private double x,y;
 
+        public Point(){
+            x=y=0;
+        }
+
+        public Point(double x,double y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public double distance(Point p){
+            double dist = Math.sqrt( Math.pow((this.x-p.getX()),2)+Math.pow((this.y-p.getY()),2));
+            return dist;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        public void setPoint(double new_x, double new_y){
+            this.x = new_x;
+            this.y = new_y;
+        }
+
+        public void shiftX(double dist){
+            this.x+=dist;
+        }
+
+        public void shiftY(double dist){
+            this.y+=dist;
+        }
+
+        public void rotate(double radian){
+            this.x = (x*(Math.cos(radian)))-(y*(Math.sin(radian)));
+            this.y = (x*(Math.sin(radian)))+(y*(Math.cos(radian)));
+        }
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("Project 1 : Point Tester\n");
